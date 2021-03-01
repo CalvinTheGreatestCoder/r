877166182a0e7913723b9e31fc22b697d740de91
CalvinTheGreatestCoder <73121416+CalvinTheGreatestCoder@users.noreply.github.com>
2021-03-01 15:11:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+.header_div 
+{ 
+  background: white; 
+color: #ce2700;
+}
+.header_div
+{
+    width: 100px
+}
+ 
+.main_div
+{
+    background: #ffd2c7;
+    padding: 10px;
+    margin-top 15px;
+    border: 3px solid white;
+ 
+}
+ 
+.main_div label 
+{
+    background: white;
+    width: 310px;
+    padding: 10px;
+    margin-bottom: 0px;
+    box-shadow: 5px 5px 18px background: white;
+ 
+}
+ 
+#camera 
+{
+    border: 2px solid white;
+    width: 310px;
+    height: 300px;
+    box-shadow: 5px 5px 18px background: white;
+ 
+}
+
